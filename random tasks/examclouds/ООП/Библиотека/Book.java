41c611394284eae3ce9b1697b8ee958aef0c2472
Nice-To-Eat-You <55923583+Nice-To-Eat-You@users.noreply.github.com>
2021-07-04 16:23:27 +0300

    Add files via upload

---
+++ random tasks/examclouds/ООП/Библиотека/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+
+    private String authorName;
+    private String bookName;
+
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+
+
+}
