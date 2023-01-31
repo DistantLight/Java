9f23cfdf6ac1d238c0a670e80b08b7b545db2238
Nice-To-Eat-You <55923583+Nice-To-Eat-You@users.noreply.github.com>
2023-01-31 21:22:02 +0300

    Delete courses directory

--- courses/lesson1/src/Animals/HomeCat.java
+++
@@ -1,7 +0,0 @@
-package Animals;
-
-public class HomeCat extends Cat{
-    public HomeCat(String name){
-        super(name);
-    }
-}
--- courses/lesson1/src/Animals/Tiger.java
+++
@@ -1,7 +0,0 @@
-package Animals;
-
-public class Tiger extends Cat{
-    public Tiger(String name){
-        super(name);
-    }
-}
